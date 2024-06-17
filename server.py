732712df2bf71214f1a732ce9cd9cdc5088de398
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-06-17 14:22:23 +0400

    Add Pyhton code to Jinja

--- server.py
+++ server.py
@@ -36,5 +36,14 @@ def guess(username):
                            )
 
 
+@app.route("/blog")
+def blog():
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(url=blog_url)
+    all_posts = response.json()
+    return render_template('blog.html', posts=all_posts)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
