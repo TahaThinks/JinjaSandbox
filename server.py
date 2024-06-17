947cd2c4268131bf82393e6ad5957f7b07dfcc7e
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-06-17 13:57:50 +0400

    Use API for Age and Gender Guess

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 import random
 from datetime import datetime
+import requests
 
+agify_url = "https://api.agify.io"
+genderize_url = "https://api.genderize.io"
 
 app = Flask(__name__)
 
@@ -14,6 +17,18 @@ def home():
                            num=random_number,
                            year=current_year)
 
+
+@app.route('/guess/<username>')
+def guess_age(username):
+    agify_parameters = {
+        'name': username
+    }
+    agify_response = requests.get(url=agify_url, params=agify_parameters)
+    genderize_response = requests.get(url=genderize_url, params=agify_parameters)
+    return f"<h1>Hey {agify_response.json()['name']}" \
+           f"<h2>I think you are {genderize_response.json()['gender']}" \
+           f"<h3> And Maybe {agify_response.json()['age']}"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
