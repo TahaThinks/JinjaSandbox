5d0fa0e62bc9e9b01c4ab51d1328c33b2c0953ef
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-06-18 21:55:53 +0400

    Jinja Final Project Add CSS

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(url=blog_url)
+    all_posts = response.json()
+    # print(all_posts)
+    return render_template("index.html", blogs=all_posts)
+
+@app.route('/blog/<blog_id>')
+def get_blog():
+    return render_template("index.html", blogs=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
